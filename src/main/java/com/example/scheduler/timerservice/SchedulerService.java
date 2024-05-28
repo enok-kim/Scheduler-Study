8bd19832199f45cf8997a9a6922597955ad8602d
enok-kim <rladpshr2581@gmail.com>
2024-05-28 15:22:04 +0900

    SchedulerService 클래스 생성 (스타트, 셧다운 메서드 생성)

---
+++ src/main/java/com/example/scheduler/timerservice/SchedulerService.java
@@ -0,0 +1,41 @@
+package com.example.scheduler.timerservice;
+
+import com.example.scheduler.jobs.HelloWorldJob;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.apache.commons.logging.Log;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulerService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SchedulerService.class);
+
+    private Scheduler scheduler;
+
+    public SchedulerService(Scheduler scheduler){
+        this.scheduler = scheduler;
+    }//SchedulerService
+
+    @PostConstruct
+    public void init(){
+        try {
+            scheduler.start();
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+        }//catch
+    }//init
+    @PreDestroy
+    public void PreDestory(){
+        try {
+            scheduler.shutdown();
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+}
