929f07bf10d98bffa089d8ce7d5831622ad054a5
enok-kim <rladpshr2581@gmail.com>
2024-05-28 16:00:14 +0900

    schedule 메서드 추가

--- src/main/java/com/example/scheduler/timerservice/SchedulerService.java
+++ src/main/java/com/example/scheduler/timerservice/SchedulerService.java
@@ -1,8 +1,12 @@
 package com.example.scheduler.timerservice;
+import com.example.scheduler.info.TimerInfo;
+import com.example.scheduler.utill.TimerUtill;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,18 @@ public class SchedulerService {
     public SchedulerService(Scheduler scheduler){
         this.scheduler = scheduler;
     }//SchedulerService
+    
+    public void schedule(final Class jobClass, final TimerInfo info){
+        final JobDetail jobDetail = TimerUtill.buildJobDetail(jobClass, info);
+        final Trigger trigger = TimerUtill.buildTrigger(jobClass, info);
+
+        try {
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+    }
 
     @PostConstruct
     public void init(){
